0d83f97c30c8762b4efe6f96df3d6119e1b9462b
Prapti Thambade <praptitambade313@gmail.com>
2025-07-15 06:58:05 +0530

    Add files via upload

---
+++ RbiBank.java
@@ -0,0 +1,10 @@
+package com.bank.service;
+
+public interface RbiBank {
+
+	public void createAccount();
+	public void deposite();
+	public void withdrawl();
+	public void check_balance();
+	public void deleteAccount();
+}
